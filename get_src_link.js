e596a22efe312c6b82a348677e9e8f04ee7dea20
doucake <ganxious@qq.com>
2017-05-12 19:45:06 +0800

    Add files via upload

--- get_src_link.js
+++ get_src_link.js
@@ -243,6 +243,7 @@ window.get_instagram_detail_link = function (aoReq) {
 		var zm_item = $("img[src='" + aoReq.img_url + "']").parents("div._tjnr4");
 		retObj.author = zm_item.find("header:eq(0)").find("div:eq(0)").find("a:eq(0)").text();
 		retObj.link = window.location.href;
+		retObj.title = "instagram @" + retObj.author +" " + document.title;
 		
 	} else {
 		var article = $("img[src='" + aoReq.img_url + "']").parents("article._h2d1o");
